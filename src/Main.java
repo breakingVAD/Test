1f1fd6714447c6f0dc5ea12cf9b9d09da2d45427
Julia Zhang <yz60@rice.edu>
2016-11-06 23:04:38 -0600

    Added printToTen method

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
     }
+    public void printToTen(){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
 }
