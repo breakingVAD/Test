ca060be2db23bf8291c7d3c12b7f623cf33cd66f
Vegetable26 <Jhwang261@gmail.com>
2016-11-06 23:04:33 -0600

    Print to ten

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
+        printToTen();
+
+    }
+
+    public static void printToTen() {
+        for (int i = 0; i < 10; i ++) {
+            System.out.println(i);
+        }
     }
 }
