8e6c33f44ca507ce6d374120010a97874cf7fac7
Vegetable26 <Jhwang261@gmail.com>
2016-11-06 23:08:14 -0600

    added print to twenty

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
-        printToTen();
+        printToTwenty();
 
     }
 
-    public static void printToTen() {
+    public static void printToTwenty() {
         for (int i = 0; i < 10; i ++) {
             System.out.println(i);
         }
